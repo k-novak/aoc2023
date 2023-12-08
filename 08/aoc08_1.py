b14512a87434f9c6423459e6584909a74d4e4acf
Krisztian Novak <krisztian.novak@gmail.com>
2023-12-08 17:35:52 +0100

    Dec/08

--- 08/aoc08_1.py
+++ 08/aoc08_1.py
@@ -18,3 +18,27 @@ LLR
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)""".strip().splitlines()
+
+with open("input.txt") as f:
+    puzzle = f.read().strip().splitlines()
+
+inp = puzzle
+
+
+nodes = {line[:3]:(line[7:10], line[12:15]) for line in inp[2:]}
+
+def instructions():
+    while 1:
+        for i in inp[0]:
+            yield i
+
+ans = 0
+cur = "AAA"
+for i in instructions():
+    ans += 1
+    L, R = nodes[cur]
+    cur = L if i == "L" else R
+    if cur == "ZZZ":
+        break
+print(ans)
+# %%
