c7236d9b511c4e1659d3eeccb4b3659bd3b687fb
Krisztian Novak <krisztian.novak@gmail.com>
2023-12-09 11:26:35 +0100

    Dec 09

---
+++ 09/aoc09_1.py
@@ -0,0 +1,29 @@
+# %%
+
+inp = """
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45
+""".strip().splitlines()
+
+with open("input.txt") as f:
+    inp = f.read().strip().splitlines()
+
+inp = [[int(value) for value in line.split()] for line in inp]
+
+ans = 0
+for i in inp:
+    hist = [i]
+    while not all(value == 0 for value in hist[-1]):
+        hist.append([b - a for a, b in zip(hist[-1][:-1], hist[-1][1:])])
+
+    hist[-1].append(0)
+    hist.reverse()
+
+    for a, b in zip(hist[:-1], hist[1:]):
+        b.append(a[-1] + b[-1])
+
+    ans += hist[-1][-1]
+
+print(ans)
+# %%
