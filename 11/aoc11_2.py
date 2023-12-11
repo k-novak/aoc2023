88ef86002bc8cbb0a864ca60f973b212ed7c83c7
Krisztian Novak <krisztian.novak@gmail.com>
2023-12-11 21:40:41 +0100

    Dec 11

--- 11/aoc11_2.py
+++ 11/aoc11_2.py
@@ -16,16 +16,12 @@ inp = """
 with open("input.txt") as f:
     inp = f.read().strip().splitlines()
 
+N = 1000000 # expansion factor
 empty_rows = [y for y, row in enumerate(inp) if '#' not in row]
 empty_cols = sorted(list(set.intersection(*[set([x for x, ch in enumerate(row) if ch == '.']) for row in inp])))
 
-# expand
-for y in reversed(empty_rows):
-    inp.insert(y, '.' * len(inp[0]))
-
-for x in reversed(empty_cols):
-    for y, row in enumerate(inp):
-        inp[y] = row[:x] + '.' + row[x:]
+X = [N if i in empty_cols else x for i, x in enumerate([1] * len(inp[0]))]
+Y = [N if i in empty_rows else y for i, y in enumerate([1] * len(inp))]
 
 
 # collect galaxies
@@ -37,9 +33,16 @@ for y in range(len(inp)):
 
 # calculate distances
 ans = 0
-for i, (x1, y1) in enumerate(G[:-1]):
-    for (x2, y2) in G[i+1:]:
-        ans += abs(x1 - x2) + abs(y1 - y2)
+for i, g1 in enumerate(G[:-1]):
+    for g2 in G[i+1:]:
+        y1, x1 = g1
+        y2, x2 = g2
+        if x1 > x2: 
+            x1, x2 = x2, x1
+        if y1 > y2: 
+            y1, y2 = y2, y1
+        res = sum(X[x1:x2]) + sum(Y[y1:y2])
+        ans += res
         
 print(ans)
 
